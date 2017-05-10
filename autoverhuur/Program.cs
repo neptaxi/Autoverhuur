b25e8c51ff1efe918449c0cad6b0f981143f6579
JOCHEM-LAPTOP\jxfir <jochem.boon.1996@gmail.com>
2017-05-10 12:07:29 +0200

    Initial push

---
+++ autoverhuur/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoverhuur
+{
+    class Program
+    {
+        List<Auto> autos = new List<Auto>();
+        List<Klant> klanten = new List<Klant>();
+
+        static void Main(string[] args)
+        {
+        }
+
+        static void invoer()
+        {
+
+        }
+
+        static void PlaatsAuto()
+        {
+
+        }
+
+        static void PlaatsKlant()
+        {
+
+        }
+
+        static void ToonAutos()
+        {
+
+        }
+
+        static void ToonKlanten()
+        {
+
+        }
+
+        static void ToonVerhuurdeAutos()
+        {
+
+        }
+
+        static void VerhuurAuto()
+        {
+
+        }
+
+        static List<Auto> GeefAutoOpVoorkeuren(double vermogen, string merk)
+        {
+            List<Auto> autosopvoorkeuren = autos;
+
+            for(int i = autosopvoorkeuren.Count-1; i>=0; i--)
+            {
+                if (!/*Voorkeur*/)
+                {
+                    autosopvoorkeuren.RemoveAt(i); //Verwijder dingen die niet aan voorkeur voldoen
+                }
+            }
+
+
+            return autosopvoorkeuren;
+        }
+
+        static void ToonAutosOpVoorkeuren()
+        {
+
+        }
+    }
+}
