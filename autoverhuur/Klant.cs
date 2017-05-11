3574c61a7ec3bdb2f089e3dbd8a7d02c523589fc
JOCHEM-LAPTOP\jxfir <jochem.boon.1996@gmail.com>
2017-05-11 10:53:20 +0200

    Klant class toegevoegd

---
+++ autoverhuur/Klant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autoverhuur
+{
+    class Klant
+    {
+        public string Naam { get; protected set; }
+        public string Adres { get; set; }
+        public int KlantNummer { get; private set; }
+
+        public Klant(string naam, string adres, int klantNummer)
+        {
+            Naam = naam;
+            Adres = adres;
+            KlantNummer = klantNummer;
+        }
+
+        public void ToonKlantGegevens()
+        {
+            Console.WriteLine("Naam {0}", Naam);
+            Console.WriteLine("Adres {0}", Adres);
+            Console.WriteLine("KlantNummer {0}", KlantNummer.ToString());
+        }
+    }
+}
